c73109b580a3100fd592e9a2de44efd1d0cf9406
JaiSehgal007 <jaisehgal511@gmail.com>
2023-06-13 03:47:14 +0530

    Before authenticate use endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator'); // this is used inorder to check for valid emails and passwords
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
 
+const JWT_SECRET='JaiIsAGoodBoy';
 
 //create a user using : POST "/api/auth/createuser", Doesn't require Auth
 router.post('/createuser', [
@@ -23,12 +26,30 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({ error: "Sorry a user with this email already exists" });
         }
+
+        const salt= await bcrypt.genSalt(10);
+        const secPass= await bcrypt.hash(req.body.password,salt);
+
+
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
-        })
-        res.json(user);
+            password: secPass,
+        });
+
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+
+        const authToken =jwt.sign(data,JWT_SECRET);
+
+
+        // res.json(user);
+
+        res.json({authToken});
+
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Some error occured");
